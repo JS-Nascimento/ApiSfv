cc78363bc3cfc69af2919cecce5c846d36776db3
JorgeInfnet <jorge.nascimento@al.infnet.edu.br>
2022-11-06 17:25:47 -0300

    Update ClientService.java

--- src/main/java/dev/jstec/apisfv/service/ClientService.java
+++ src/main/java/dev/jstec/apisfv/service/ClientService.java
@@ -1,5 +1,6 @@
 package dev.jstec.apisfv.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import dev.jstec.apisfv.model.Client;
@@ -7,11 +8,17 @@ import dev.jstec.apisfv.repository.ClientRepository;
 
 @Service
 public class ClientService {
+	
+	private ClientRepository repository;
+	
+	@Autowired
+	public ClientService(ClientRepository repository) {
+		this.repository = repository;
+	}
 
 	public void saveClient(Client client) {
 		ClientValidate(client);
-		ClientRepository clientRepository = new ClientRepository();
-		clientRepository.Persist(client);
+		this.repository.Persist(client);
 	}
 	
 	public void ClientValidate(Client client) {
